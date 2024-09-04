01a8676e6b3f4b0f010773752ea10d9b5df01e08
2400172 <116487866+Miru1218@users.noreply.github.com>
2024-09-04 16:14:50 +0800

    增加 del

--- hello/src/hello/index.ts
+++ hello/src/hello/index.ts
@@ -1,17 +1,20 @@
 import { Rule, SchematicContext, Tree } from '@angular-devkit/schematics';
 
-
-// You don't have to export the function as default. You can also have more than one rule factory
-// per file.
 export function hello(_options: any): Rule {
   return (tree: Tree, _context: SchematicContext) => {
-    //   console.log('Hello world');
-    //   tree.create('hello.js', `console.log('Hello World');`);
-    //   return tree;
-    // };
     const { name } = _options; // 從 _options 讀取 name
 
-    tree.create("hello.js", `console.log('Hello ${name}');`);
+    // 定義文件的相對路徑
+    const filePath = 'hello.js';
+
+    // 如果文件已經存在，先刪除它
+    if (tree.exists(filePath)) {
+      tree.delete(filePath);
+    }
+
+    // 創建新的 hello.js 文件
+    tree.create(filePath, `console.log('Hello ${name}');11111111111111111111111111`);
+
     return tree;
   };
 }
